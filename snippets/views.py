783b485f8dd13fb05ce7cc3cda78cb32a32d8d3d
Jiyoung Hwang <jiyoung@repick.co>
2017-07-05 15:26:09 +0900

    Serialization

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+# django 의 POST request는 원래 csrf 를 통과해야하는데, 다른 endpoint를 거쳐올 경우, 이걸 할 수 없기 때문에 일단은 csrf_exempt를 한다.
+@csrf_exempt
+def snippet_list(request):
+    """
+    request가 들어오면, snippet들의 리스트를 보여주거나, 새로운 snippet을 만든다.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        # We can also serialize querysets instead of model instances. To do so we simply add a many=True flag to the serializer arguments.
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        # data를 JSON 데이터타입으로 serialize한다. 타입(charfield, text, boolean...)은 ModelSerializer>models.py에 저장되어 있다.
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
